4e31912e9b8e16e9d39a25c0f12dd6a92b1446d6
RamónMN <ramon.martinez.nieto@gmail.com>
2024-01-22 19:04:19 +0100

    refactor: delete identation

--- LeapYear/Year.cs
+++ LeapYear/Year.cs
@@ -12,14 +12,9 @@ public class Year
 
     public bool IsLeap()
     {
-        if (Value % 4 == 0)
-        {
-            if (Value % 400 == 0) return true;
-            if (Value % 100 == 0) return false;
-
-            return true;
-        }
-
+        if (Value % 400 == 0) return true;
+        if (Value % 100 == 0) return false;
+        if (Value % 4 == 0) return true;
         return false;
     }
 }
