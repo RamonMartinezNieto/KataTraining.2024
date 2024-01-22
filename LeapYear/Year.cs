192ff507ce2e303d5c364df4209b4dba43fae91e
RamónMN <ramon.martinez.nieto@gmail.com>
2024-01-22 18:57:07 +0100

    refactor: clean code

--- LeapYear/Year.cs
+++ LeapYear/Year.cs
@@ -12,19 +12,10 @@ public class Year
 
     public bool IsLeap()
     {
-
-
         if (Value % 4 == 0)
         {
-            if (Value % 400 == 0)
-            {
-                return true;
-            }
-
-            if (Value % 100 == 0)
-            {
-                return false;
-            }
+            if (Value % 400 == 0) return true;
+            if (Value % 100 == 0) return false;
 
             return true;
         }
