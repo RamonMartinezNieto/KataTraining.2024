3082262a178ca37604eb8195c3c37addf4f24dc8
RamónMN <ramon.martinez.nieto@gmail.com>
2024-01-22 18:59:47 +0100

    refactor: use english by default to write US

--- LeapYear.Unit.Tests/LeapYearShould.cs
+++ LeapYear.Unit.Tests/LeapYearShould.cs
@@ -3,11 +3,16 @@ using Xunit;
 
 namespace LeapYear.Unit.Tests;
 
-// Kata LeapYear
-// Los años no divisibles por 4 no son bisiestos(como 1997).
-// Los años divisibles por 4 son bisiestos(como 1996), excepto:
-// Si son divisibles por 100 no son bisiestos(como 1900).
-// Si son divisibles por 400 serán bisiestos(como 2000).
+//Kata LeapYears
+//User Story: 
+//As a user, I want to know if a year is a leap year,
+//So that I can plan for an extra day on February 29th during those years.
+
+//Acceptance Criteria:
+//All years divisible by 400 ARE leap years(so, for example, 2000 was indeed a leap year),
+//All years divisible by 100 but not by 400 are NOT leap years(so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+//All years divisible by 4 but not by 100 ARE leap years(e.g., 2008, 2012, 2016),
+//All years not divisible by 4 are NOT leap years(e.g. 2017, 2018, 2019).
 
 public class LeapYearShould
 {
