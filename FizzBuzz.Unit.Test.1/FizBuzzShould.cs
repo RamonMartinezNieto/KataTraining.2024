67fd645b4c1d8819d7aed8405c96d65fbfee29a9
RamónMN <ramon.martinez.nieto@gmail.com>
2024-01-24 18:06:05 +0100

    kata fizzBuzz 2

--- FizzBuzz.Unit.Test.1/FizBuzzShould.cs
+++ FizzBuzz.Unit.Test.1/FizBuzzShould.cs
@@ -5,7 +5,6 @@ using Xunit;
 //Write a program that prints the numbers from 1 to 100. 
 //But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 //For numbers which are multiples of both three and five print “FizzBuzz “.
-//Stage 2 - new requirements
 
 namespace FizzBuzz.Unit.Test._1;
 
